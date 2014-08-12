d85861375f7759178cd4a867fb81ff194a5de58b
bspatafora <benjamin.spatafora@gmail.com>
2014-08-12 09:46:30 -0500

    Test arguments passed from controller actions to TicTacToes in specs

--- spec/controllers/game_controller_spec.rb
+++ spec/controllers/game_controller_spec.rb
@@ -1,12 +1,17 @@
 require 'rails_helper'
 require 'tic_tac_toes/ui/adapter'
+require 'tic_tac_toes/ui/serializer'
 
 describe GameController, :type => :controller do
   describe 'GET #start_game' do
     it 'sends the adapter #start_game' do
-      computer_type = "EASY_AI"
-      expect(TicTacToes::UI::Adapter).to receive(:start_game)
-      get :start_game, { computer_type: computer_type }
+      computer_type = 'EASY_AI'
+      order = 'first'
+      game_state = double
+      allow(TicTacToes::UI::Serializer).to receive(:game_state_from_board_structure) { game_state }
+
+      expect(TicTacToes::UI::Adapter).to receive(:start_game).with(order, game_state, GameController)
+      get :start_game, { computer_type: computer_type, order: order }
     end
   end
 
@@ -14,8 +19,11 @@ describe GameController, :type => :controller do
     it 'sends the adapter #make_move' do
       board = [nil, nil, nil, nil, nil, nil, nil, nil, nil]
       move = '0'
-      computer_type = "EASY_AI"
-      expect(TicTacToes::UI::Adapter).to receive(:make_move)
+      computer_type = 'EASY_AI'
+      game_state = double
+      allow(TicTacToes::UI::Serializer).to receive(:game_state_from_board_structure) { game_state }
+
+      expect(TicTacToes::UI::Adapter).to receive(:make_move).with(game_state, move, GameController)
       get :move, { board: board, move: move, computer_type: computer_type }
     end
   end
