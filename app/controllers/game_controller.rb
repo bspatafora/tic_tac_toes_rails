f7a953ac33476eb850f8c841ca390c8694e7c9c7
bspatafora <benjamin.spatafora@gmail.com>
2014-08-25 16:02:43 -0500

    Change computer_type to ai_type

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -4,10 +4,10 @@ require 'tic_tac_toes/ui/serializer'
 
 class GameController < ApplicationController
   def start_game
-    if params[:computer_type] && params[:order]
+    if params[:ai_type] && params[:order]
       board = TicTacToes::UI::Serializer.new_board_structure
       game_state = TicTacToes::UI::Serializer.game_state_from_board_structure(board,
-                                                                              params[:computer_type])
+                                                                              params[:ai_type])
       TicTacToes::UI::Adapter.start_game(params[:order], game_state, self)
     else
       flash.now[:alert] = translate(:incomplete_form_alert)
@@ -17,13 +17,13 @@ class GameController < ApplicationController
 
   def move
     game_state = TicTacToes::UI::Serializer.game_state_from_board_structure(params[:board],
-                                                                            params[:computer_type])
+                                                                            params[:ai_type])
     TicTacToes::UI::Adapter.make_move(game_state, params[:move], self)
   end
 
   def moves_were_made(game_state)
     @board = TicTacToes::UI::Serializer.board_structure_from_game_state(game_state)
-    @computer_type = TicTacToes::UI::Serializer.computer_type_from_game_state(game_state)
+    @ai_type = TicTacToes::UI::Serializer.ai_type_from_game_state(game_state)
     @predictions = JSON.generate({ spaces: TicTacToes::UI::Adapter.predictions(game_state) })
     render :board
   end
--- spec/controllers/game_controller_spec.rb
+++ spec/controllers/game_controller_spec.rb
@@ -5,13 +5,13 @@ require 'tic_tac_toes/ui/serializer'
 describe GameController, :type => :controller do
   describe 'GET #start_game' do
     it 'sends the adapter #start_game' do
-      computer_type = 'EASY_AI'
+      ai_type = 'EASY_AI'
       order = 'first'
       game_state = double
       allow(TicTacToes::UI::Serializer).to receive(:game_state_from_board_structure) { game_state }
 
       expect(TicTacToes::UI::Adapter).to receive(:start_game).with(order, game_state, GameController)
-      get :start_game, { computer_type: computer_type, order: order }
+      get :start_game, { ai_type: ai_type, order: order }
     end
   end
 
@@ -19,12 +19,12 @@ describe GameController, :type => :controller do
     it 'sends the adapter #make_move' do
       board = [nil, nil, nil, nil, nil, nil, nil, nil, nil]
       move = '0'
-      computer_type = 'EASY_AI'
+      ai_type = 'EASY_AI'
       game_state = double
       allow(TicTacToes::UI::Serializer).to receive(:game_state_from_board_structure) { game_state }
 
       expect(TicTacToes::UI::Adapter).to receive(:make_move).with(game_state, move, GameController)
-      get :move, { board: board, move: move, computer_type: computer_type }
+      get :move, { board: board, move: move, ai_type: ai_type }
     end
   end
 end
